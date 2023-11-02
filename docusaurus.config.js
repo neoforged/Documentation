a8806dface5032078ad2a2885a558895851e6992
Mysterious_Dev <mysteriousdevs@gmail.com>
2023-11-02 10:50:14 +0100

    Upgrade to Docusaurus v3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
-
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const {themes} = require('prism-react-renderer');
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -171,8 +171,8 @@ const config = {
         copyright: `Copyright © 2016, under the MIT license. Built with Docusaurus.`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: lightTheme,
+        darkTheme: darkTheme,
         additionalLanguages: ["java", "gradle", "toml", "groovy", "kotlin"],
       },
       algolia: {
